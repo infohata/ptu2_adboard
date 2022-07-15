cabbd4a8f1d1b1b34495991fedcd717b3c24bd9f
Kestutis Januskevicius <infohata@gmail.com>
2022-07-15 14:55:55 +0300

    basic user registration form

--- adboard/user_menu/views.py
+++ adboard/user_menu/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model, forms as user_forms
+from django.views import generic
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class UserCreate(generic.CreateView):
+    form_class = user_forms.UserCreationForm
+    success_url = reverse_lazy('sign_up_welcome')
+    template_name = 'registration/user_create_form.html'
+
+
+class UserCreateDone(generic.TemplateView):
+    template_name = 'registration/user_create_done.html'
